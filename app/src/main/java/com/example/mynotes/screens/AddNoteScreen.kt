66fb2e557246cb7f6b5a288ed5c324125d579778
tito06 <prabalkarid@gmail.com>
2024-05-24 12:59:56 +0530

    add note page ui update category added

--- app/src/main/java/com/example/mynotes/screens/AddNoteScreen.kt
+++ app/src/main/java/com/example/mynotes/screens/AddNoteScreen.kt
@@ -1,16 +1,38 @@
 package com.example.mynotes.screens
 
+import android.annotation.SuppressLint
 import android.os.Build
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,16 +40,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.mynotes.NavScreen
 import com.example.mynotes.NoteViewModel
 import com.example.mynotes.db.NotesEntity
+import com.example.mynotes.popinFamily
+import com.example.mynotes.popinFamilyNormal
+import com.example.mynotes.popinFamilyTitle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.time.LocalDate
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AddNoteScreen(
@@ -39,40 +75,159 @@ fun AddNoteScreen(
     var noteTitle by remember { mutableStateOf("") }
     var noteContent by remember { mutableStateOf("") }
     var date = LocalDate.now()
+    var showAllMenu by remember { mutableStateOf(false) }
+    var buttonPosition by remember { mutableStateOf(Offset.Zero) }
+    var buttonSize by remember { mutableStateOf(Size.Zero) }
+    val context = LocalContext.current
+    val items  = listOf("Personal", "Work", "Grocery", "Shopping")
+    var selectedItem by remember {
+        mutableStateOf(items[0])
+    }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { androidx.compose.material3.Text(text = "New note",
+                fontFamily = popinFamilyTitle)})
+        }
     ) {
-        TextField(
-            value = noteTitle,
-            onValueChange = { noteTitle = it },
-            label = { Text(text = "Title") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        TextField(
-            value = noteContent,
-            onValueChange = { noteContent = it },
-            label = { Text(text = "Content") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                val newNote = NotesEntity(title = noteTitle, content = noteContent, date = date)
-                noteViewModel.insert(newNote)
-                navController.navigate(NavScreen.NoteListScreen.route)
-               // navController.popBackStack()
-            },
-            modifier = Modifier.align(Alignment.End)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp, 80.dp, 16.dp, 0.dp)
         ) {
-            Text(text = "Save")
+            OutlinedTextField(
+                value = noteTitle,
+                onValueChange = { noteTitle = it },
+                label = { Text(text = "Title") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .padding(4.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                ),
+                border = BorderStroke(width = 1.dp, Color.Black)
+            ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    selectedItem,
+                    fontFamily = popinFamily,
+                    modifier = Modifier.padding(14.dp, 0.dp)
+                )
+
+                IconButton(
+                    onClick = {
+                        showAllMenu = !showAllMenu
+                    }, modifier = Modifier.onGloballyPositioned {
+                        buttonSize = it.size.toSize()
+                        buttonPosition = it.positionInRoot()
+                    },
+                    colors = IconButtonDefaults.iconButtonColors(Color.Red)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Down arrow"
+                    )
+                }
+            }
+                    // Show the DropdownMenu
+                    if (showAllMenu) {
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentSize(Alignment.TopEnd)
+                        ) {
+                            DropdownMenu(
+                                expanded = showAllMenu,
+                                onDismissRequest = { showAllMenu = false },
+                                /*  offset = DpOffset(
+                                x = with(LocalDensity.current) { buttonPosition.x.toDp() },
+                                y = with(LocalDensity.current) { (buttonPosition.y).toDp() }
+                            )*/
+
+
+                            ) {
+                                items.forEach {
+                                    DropdownMenuItem(
+                                        text = { androidx.compose.material3.Text(text = it) },
+                                        onClick = {
+                                            selectedItem = it
+                                            showAllMenu = false
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .padding(4.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                ),
+                border = BorderStroke(width = 1.dp, Color.Black)
+            ) {
+
+                TextField(
+                    value = noteContent,
+                    onValueChange = { noteContent = it },
+                    label = { Text(text = "Content") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                )
+
+            }
+
+
+
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+
+
+            Button(
+                onClick = {
+                    if (noteTitle.isNotEmpty() && noteContent.isNotEmpty()) {
+                        val newNote =
+                            NotesEntity(title = noteTitle, content = noteContent, date = date)
+                        noteViewModel.insert(newNote)
+                        navController.navigate(NavScreen.NoteListScreen.route)
+                    }else{
+                        Toast.makeText(context , "Can not save a blank file.", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // navController.popBackStack()
+                },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = "Save")
+            }
         }
     }
+
+
 }
